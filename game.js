0dfb8e35cbb11d9bb0354fc9fcb5ed77e0883067
Chai Phonbopit <phonbopit@gmail.com>
2014-08-19 15:02:07 +0700

    complete handler keyboard&mouse

--- game.js
+++ game.js
@@ -11,11 +11,27 @@ BasicGame.Game.prototype = {
 
     // Load enemy image.
     this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
+
+    // Load explosion
+    this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+
+    // Load Player
+    this.load.spritesheet('player', 'assets/player.png', 64, 64);
+
   },
 
   create: function () {
 
     this.sea = this.add.tileSprite(0, 0, 1024, 768, 'sea');
+
+    // setup player
+    this.player = this.add.sprite(400, 650, 'player');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('fly', [0, 1, 2], 20, true);
+    this.player.play('fly');
+    this.physics.enable(this.player, Phaser.Physics.ARCADE);
+    this.player.speed = 300;
+    this.player.body.collideWorldBounds = true;
 
     // after load enemy, now i can use it.
     this.enemy = this.add.sprite(512, 300, 'greenEnemy');
@@ -30,7 +46,9 @@ BasicGame.Game.prototype = {
 
     //Apply Phisics
     this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
-    this.bullet.body.velocity.y = -400;
+    this.bullet.body.velocity.y = -200;
+
+    this.cursors = this.input.keyboard.createCursorKeys();
   },
 
   update: function () {
@@ -42,16 +60,46 @@ BasicGame.Game.prototype = {
       null,
       this
     );
+
+    
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
+    // Handling all keyboard input (keyDown)
+    // Left & Right 
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x = -this.player.speed;
+    } else if (this.cursors.right.isDown) {
+      this.player.body.velocity.x = this.player.speed;
+    }
+
+    // Up & Down 
+    if (this.cursors.up.isDown) {
+      this.player.body.velocity.y = -this.player.speed;
+    } else if (this.cursors.down.isDown) {
+      this.player.body.velocity.y = this.player.speed;
+    }
+
+    // Handler Mouse Click & Touch
+    if (this.input.activePointer.isDown &&
+        this.physics.arcade.distanceToPointer(this.player) > 15) {
+      this.physics.arcade.moveToPointer(this.player, this.player.speed);
+    }
+
   },
 
   render: function() {
-    this.game.debug.body(this.bullet);
-    this.game.debug.body(this.enemy);
+/*    this.game.debug.body(this.bullet);
+    this.game.debug.body(this.enemy);*/
   },
 
   enemyHit: function(bullet, enemy) {
     bullet.kill();
     enemy.kill();
+    var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion');
+    explosion.anchor.setTo(0.5, 0.5);
+    explosion.animations.add('boom');
+    explosion.play('boom', 15, false, true);
   },
 
   quitGame: function (pointer) {
